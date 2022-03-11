9a867823358ef37beb7446b6f41e2c0ccf52f096
Pro465 <namitapro0987@gmail.com>
2022-03-11 17:52:51 +0530

    add more features

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
-use std::any::Any;
 use std::panic;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::sync::Mutex;
 use std::thread::{self, JoinHandle};
 
 struct Wrapper<T>(*mut T);
 
 unsafe impl<T> Send for Wrapper<T> {}
 
-pub fn buffer<U, D, B>(bufs: &mut [B], mut update: U, mut draw: D) -> !
+pub type Res<E> = Result<(), E>;
+
+pub enum Error<U, D> {
+    FromUpdate(U),
+    FromDraw(D),
+}
+
+pub fn buffer<U, D, B, R1, R2>(bufs: &mut [B], mut update: U, mut draw: D) -> Error<R1, R2>
 where
-    U: FnMut(&mut B),
-    D: FnMut(&B) + Send,
+    U: FnMut(&mut B) -> Res<R1>,
+    D: FnMut(&B) -> Res<R2> + Send,
+    R2: Send,
 {
     let len = bufs.len();
     let bufs = bufs.as_mut_ptr();
@@ -19,6 +27,7 @@ where
     let ui = &AtomicUsize::new(0);
     let di = &AtomicUsize::new(0);
     let stop = &AtomicBool::new(false);
+    let ret = &Mutex::new(None::<R2>);
 
     let handle = {
         let closure = move || {
@@ -39,9 +48,12 @@ where
 
                 // SAFETY: we already waited until the main thread was done, so the only way this
                 // could be acceessed simultanously is if the main thread tried to access it while
-                // we are `draw`ing, which ruled out by the other thread checking to see if the
+                // we are `draw`ing, which is ruled out by the other thread checking to see if the
                 // next item its gonna modify is being accessed by us and waiting until we are done
-                (draw)(unsafe { &*bufs.add(di_val) });
+                if let Err(x) = (draw)(unsafe { &*bufs.add(di_val) }) {
+                    *ret.lock().unwrap() = Some(x);
+                    return;
+                }
 
                 di.store(di_val + 1 % len, Ordering::SeqCst);
             }
@@ -57,14 +69,26 @@ where
     loop {
         let ui_val = ui.load(Ordering::SeqCst);
 
+        if let Some(x) = ret.lock().unwrap().take() {
+            return Error::FromDraw(x);
+        }
+
         let res = panic::catch_unwind(panic::AssertUnwindSafe(|| {
             // SAFETY: we are the only ones accessing this because if the other thread is trying
             // to access the same buf as us, it will wait until we are finished
             (update)(unsafe { &mut *bufs.add(ui_val) })
         }));
 
-        if let Err(e) = res {
-            handle_panic(e, handle, stop);
+        match res {
+            Err(e) => {
+                join(handle, stop);
+                panic::resume_unwind(e);
+            }
+            Ok(Err(x)) => {
+                join(handle, stop);
+                return Error::FromUpdate(x);
+            }
+            _ => {}
         }
 
         while ui_val + 1 % len == di.load(Ordering::SeqCst) {
@@ -75,13 +99,11 @@ where
     }
 }
 
-fn handle_panic(e: Box<dyn Any + Send + 'static>, h: JoinHandle<()>, stop: &AtomicBool) -> ! {
+fn join(h: JoinHandle<()>, stop: &AtomicBool) {
     stop.store(true, Ordering::SeqCst);
 
     #[allow(unused_must_use)]
     {
         h.join();
     }
-
-    panic::resume_unwind(e);
 }
