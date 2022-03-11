21d87f20c8fa72adaf9594e7071e104544503535
Pro465 <namitapro0987@gmail.com>
2022-03-11 16:59:27 +0530

    included SAFETY comments

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,12 @@ use std::panic;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use std::thread::{self, JoinHandle};
 
-struct Wrapper<T: Send>(*mut T);
+struct Wrapper<T>(*mut T);
 
-unsafe impl<T: Send> Send for Wrapper<T> {}
+unsafe impl<T> Send for Wrapper<T> {}
 
 pub fn buffer<U, D, B>(bufs: &mut [B], mut update: U, mut draw: D) -> !
 where
-    B: Send,
     U: FnMut(&mut B),
     D: FnMut(&B) + Send,
 {
@@ -23,6 +22,8 @@ where
 
     let handle = {
         let closure = move || {
+            // SAFETY: we are only accessing different locations from different threads at any
+            // time, so this, in fact is safe to be Send
             let bufs = wrapper.0;
 
             loop {
@@ -36,22 +37,29 @@ where
                     return;
                 }
 
+                // SAFETY: we already waited until the main thread was done, so the only way this
+                // could be acceessed simultanously is if the main thread tried to access it while
+                // we are `draw`ing, which ruled out by the other thread checking to see if the
+                // next item its gonna modify is being accessed by us and waiting until we are done
                 (draw)(unsafe { &*bufs.add(di_val) });
 
                 di.store(di_val + 1 % len, Ordering::SeqCst);
             }
         };
 
-        let b: Box<dyn FnOnce() + Send> = Box::new(closure);
-        let b: Box<dyn FnOnce() + Send + 'static> = unsafe { std::mem::transmute(b) };
+        let f: Box<dyn FnOnce() + Send> = Box::new(closure);
+        // SAFETY: we will close this thread before returning from this function's scope
+        let f: Box<dyn FnOnce() + Send + 'static> = unsafe { std::mem::transmute(f) };
 
-        thread::spawn(move || b())
+        thread::spawn(move || f())
     };
 
     loop {
         let ui_val = ui.load(Ordering::SeqCst);
 
         let res = panic::catch_unwind(panic::AssertUnwindSafe(|| {
+            // SAFETY: we are the only ones accessing this because if the other thread is trying
+            // to access the same buf as us, it will wait until we are finished
             (update)(unsafe { &mut *bufs.add(ui_val) })
         }));
 
