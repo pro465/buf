bf09a4be6eebead37120cb5c429edb85ab5ee5f5
Pro465 <namitapro0987@gmail.com>
2022-10-08 12:41:54 +0530

    make it safer

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
 use std::panic;
-use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Mutex;
-use std::thread::{self, JoinHandle};
-
-struct Wrapper<T>(*mut T);
-
-unsafe impl<T> Send for Wrapper<T> {}
+use std::thread::{self, ScopedJoinHandle};
 
 pub type Res<E> = Result<(), E>;
 
@@ -14,97 +10,73 @@ pub enum Error<U, D> {
     FromDraw(D),
 }
 
-pub fn buffer<U, D, B, R1, R2>(bufs: &mut [B], mut update: U, mut draw: D) -> Error<R1, R2>
+pub fn buffer<U, D, B, R1, R2>(bufs: &mut [Mutex<B>], mut update: U, mut draw: D) -> Error<R1, R2>
 where
     U: FnMut(&mut B) -> Res<R1>,
     D: FnMut(&mut B) -> Res<R2> + Send,
     R2: Send,
+    B: Send,
 {
     let len = bufs.len();
-    let bufs = bufs.as_mut_ptr();
-    let wrapper = Wrapper(bufs);
+    let stop = AtomicBool::new(false);
+    let flag = AtomicBool::new(false);
+    let ret = Mutex::new(None::<R2>);
 
-    let ui = &AtomicUsize::new(0);
-    let di = &AtomicUsize::new(0);
-    let stop = &AtomicBool::new(false);
-    let ret = &Mutex::new(None::<R2>);
+    thread::scope(|s| {
+        let handle = s.spawn(|| {
+            while !flag.load(Ordering::Relaxed) {
+                thread::park();
+            }
 
-    let handle = {
-        let closure = move || {
-            // SAFETY: we are only accessing different locations from different threads at any
-            // time, so this, in fact is safe to be Send
-            let bufs = wrapper.0;
+            let mut di = 0;
 
             loop {
-                let di_val = di.load(Ordering::SeqCst);
-
-                if stop.load(Ordering::SeqCst) {
+                if stop.load(Ordering::Relaxed) {
                     return;
                 }
 
-                while di_val == ui.load(Ordering::SeqCst) {
-                    if stop.load(Ordering::SeqCst) {
-                        return;
-                    }
-
-                    std::hint::spin_loop();
-                }
-
-                // SAFETY: we already waited until the main thread was done, so the only way this
-                // could be acceessed simultanously is if the main thread tried to access it while
-                // we are `draw`ing, which is ruled out by the other thread checking to see if the
-                // next item its gonna modify is being accessed by us and waiting until we are done
-                if let Err(x) = (draw)(unsafe { &mut *bufs.add(di_val) }) {
+                if let Err(x) = (draw)(&mut *bufs[di].lock().unwrap()) {
                     *ret.lock().unwrap() = Some(x);
                     return;
                 }
 
-                di.store((di_val + 1) % len, Ordering::SeqCst);
+                di = (di + 1) % len;
             }
-        };
+        });
 
-        let f: Box<dyn FnOnce() + Send> = Box::new(closure);
-        // SAFETY: we will close this thread before returning from this function's scope
-        let f: Box<dyn FnOnce() + Send + 'static> = unsafe { std::mem::transmute(f) };
+        let mut ui = 0;
 
-        thread::spawn(move || f())
-    };
-
-    loop {
-        let ui_val = ui.load(Ordering::SeqCst);
-
-        if let Some(x) = ret.lock().unwrap().take() {
-            return Error::FromDraw(x);
-        }
-
-        let res = panic::catch_unwind(panic::AssertUnwindSafe(|| {
-            // SAFETY: we are the only ones accessing this because if the other thread is trying
-            // to access the same buf as us, it will wait until we are finished
-            (update)(unsafe { &mut *bufs.add(ui_val) })
-        }));
-
-        match res {
-            Err(e) => {
-                join(handle, stop);
-                panic::resume_unwind(e);
+        loop {
+            if let Some(x) = ret.lock().unwrap().take() {
+                return Error::FromDraw(x);
             }
-            Ok(Err(x)) => {
-                join(handle, stop);
-                return Error::FromUpdate(x);
+
+            let res = panic::catch_unwind(panic::AssertUnwindSafe(|| {
+                (update)(&mut *bufs[ui].lock().unwrap())
+            }));
+
+            match res {
+                Err(e) => {
+                    join(handle, &stop);
+                    panic::resume_unwind(e);
+                }
+                Ok(Err(x)) => {
+                    join(handle, &stop);
+                    return Error::FromUpdate(x);
+                }
+                _ => {}
             }
-            _ => {}
-        }
 
-        while (ui_val + 1) % len == di.load(Ordering::SeqCst) {
-            std::hint::spin_loop();
+            ui = (ui + 1) % len;
+            if !flag.swap(true, Ordering::Relaxed) {
+                handle.thread().unpark();
+            }
         }
-
-        ui.store((ui_val + 1) % len, Ordering::SeqCst);
-    }
+    })
 }
 
-fn join(h: JoinHandle<()>, stop: &AtomicBool) {
-    stop.store(true, Ordering::SeqCst);
+fn join(h: ScopedJoinHandle<()>, stop: &AtomicBool) {
+    stop.store(true, Ordering::Relaxed);
 
     #[allow(unused_must_use)]
     {
